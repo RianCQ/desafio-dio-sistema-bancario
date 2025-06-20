bffcfb8af478488bc42e632848ebc27cc1c05e9c
Rian Cesar Quintanilha <rianquintanilha5@gmail.com>
2025-06-19 21:08:03 -0300

    adiciona opções de nova conta e novo usuário ao menu refatora função de saque para incluir controle de extrato e número de saques corrige variáveis de controle de saques

--- desafio.py
+++ desafio.py
@@ -3,14 +3,19 @@ menu = """
   [d] Depositar
   [s] Sacar
   [e] Extrato
+  [nc] Nova conta
+  [nu] Novo usuário
   [p] Parar
 
   => """
+'''
 saldo = 0
 limite = 500
-nSaques = 3
+limite_saques = 3
+numero_saques = 0
 depositos = []
-saques = []
+#saques = []
+'''
 
 def deposito(saldo: float, valor: float, depositos: list):
     if valor > 0.0:
@@ -21,16 +26,20 @@ def deposito(saldo: float, valor: float, depositos: list):
         print("Valor inválido para depósito.")
     return saldo
 
-def saque(saldo, valor: float, saques: list):
-    if len(saques) < nSaques and valor <= limite :
-        if saldo > valor :
-            saldo = saldo - valor
-            saques.append(valor)
-            print("Saque realizado com sucesso.")
-        else :
-            print("Erro: Saldo insuficiente.")
-    else :
-        print("Erro: Saque negado.")
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    if numero_saques < limite_saques :
+        if valor <= limite:
+            if saldo > valor :
+                saldo = saldo - valor
+                extrato += f"Saque: R${valor:.2f}\n"
+                numero_saques += 1
+                print("Saque realizado com sucesso.")
+            else :
+                print("Erro: Saldo insuficiente.")
+        else:
+            print("Erro: Valor de saldo excede o limite de retirada.")
+    else:
+        print("Erro: Limite de saques diários atingido.")
     return saldo
 
 def extrato(saldo: float, depositos, saques: list) -> None:
@@ -53,19 +62,30 @@ def extrato(saldo: float, depositos, saques: list) -> None:
         print(f"> Saldo total: R${saldo:.2f}")
     print("###########################################")
 
-while True:
-    opcao = input(menu)
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
 
-    if opcao == 'd':
-        valor = float(input("Qual o valor de deposito? "))
-        saldo = deposito(saldo, valor, depositos)
-    elif opcao == 's':
-        valor = float(input("Qual o valor de saque? "))
-        saldo = saque(saldo, valor, saques)
-    elif opcao == 'e':
-        extrato(saldo, depositos, saques)
-    elif opcao == 'p':
-        print("Saindo do processo...")
-        break
-    else:
-        print("Erro: Operação inválida. Por favor, selecione novamente a operação desejada.")
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+        opcao = input(menu)
+
+        if opcao == 'd':
+            valor = float(input("Qual o valor de deposito? "))
+            saldo = deposito(saldo, valor, depositos)
+        elif opcao == 's':
+            valor = float(input("Qual o valor de saque? "))
+            saldo = saque(saldo=saldo, valor=valor, saques=saques)
+        elif opcao == 'e':
+            extrato(saldo, depositos, saques)
+        elif opcao == 'p':
+            print("Saindo do processo...")
+            break
+        else:
+            print("Erro: Operação inválida. Por favor, selecione novamente a operação desejada.")
